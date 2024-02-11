f951c2fd300c4c4b011146dfe5083d576c321a01
danpilnet <149692136+danpilnet@users.noreply.github.com>
2024-02-11 23:30:04 +0900

    Add files via upload

---
+++ project/sign/views.py
@@ -0,0 +1,16 @@
+
+
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+from .models import BaseRegisterForm
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'protect/index.html'
+
+
+class BaseRegisterView(CreateView):
+    model = User
+    form_class = BaseRegisterForm
+    success_url = '/'
